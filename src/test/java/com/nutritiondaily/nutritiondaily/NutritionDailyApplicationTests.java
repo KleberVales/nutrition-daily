e57a9bdeb169913595b9716fdf0bf3cd6332e487
Kleber Vales <klebervales.dev@gmail.com>
2026-04-18 16:13:30 -0300

    Initialize NutritionDaily Spring Boot application with main class and basic test.

---
+++ src/test/java/com/nutritiondaily/nutritiondaily/NutritionDailyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nutritiondaily.nutritiondaily;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NutritionDailyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
