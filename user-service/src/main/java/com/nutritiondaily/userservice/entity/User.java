5ae93c6a315edc3a969920a5684bf0cacd322e8b
Kleber Vales <klebervales.dev@gmail.com>
2026-05-10 10:12:38 -0300

    Add User entity class with JavaDoc comments

---
+++ user-service/src/main/java/com/nutritiondaily/userservice/entity/User.java
@@ -0,0 +1,13 @@
+package com.nutritiondaily.userservice.entity;
+
+
+/**
+ * The User class represents a user in the system.
+ * It may include properties such as username, password, and other personal details,
+ * and is designed to store and manage user-related information.
+ *
+ * This class can be used as a foundation for user authentication, authorization,
+ * and other user-related features in an application.
+ */
+public class User {
+}
