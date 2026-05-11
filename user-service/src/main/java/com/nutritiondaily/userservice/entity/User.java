90a0eb186b849b172e4121e0a5be0a816d435705
Kleber Vales <klebervales.dev@gmail.com>
2026-05-11 10:07:15 -0300

    Annotate `User` entity with JPA annotations, add new fields, and update build.gradle with dependencies for JPA and Lombok

--- user-service/src/main/java/com/nutritiondaily/userservice/entity/User.java
+++ user-service/src/main/java/com/nutritiondaily/userservice/entity/User.java
@@ -1,6 +1,13 @@
 package com.nutritiondaily.userservice.entity;
 
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import java.util.UUID;
+
 /**
  * The User class represents a user in the system.
  * It may include properties such as username, password, and other personal details,
@@ -9,42 +16,19 @@ package com.nutritiondaily.userservice.entity;
  * This class can be used as a foundation for user authentication, authorization,
  * and other user-related features in an application.
  */
+
+@Entity
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
 
     private String username;
     private String password;
     private String email;
+    private int age;
+    private String sex;
 
 
-    public User() {
-    }
-
-    public User(String username, String password) {
-        this.username = username;
-        this.password = password;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    @Override
-    public String toString() {
-        return "User{" +
-                "username='" + username + '\'' +
-                ", password='" + password + '\'' +
-                '}';
-    }
 }
