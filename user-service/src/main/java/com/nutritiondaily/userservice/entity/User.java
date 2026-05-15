7b0af45e13b8eb699d6a1edfdf7c1998a09a0075
Kleber Vales <klebervales.dev@gmail.com>
2026-05-15 17:10:38 -0300

    Annotate `User` entity with JPA annotations, add new fields, and update build.gradle with dependencies for JPA and Lombok

--- user-service/src/main/java/com/nutritiondaily/userservice/entity/User.java
+++ user-service/src/main/java/com/nutritiondaily/userservice/entity/User.java
@@ -8,15 +8,6 @@ import jakarta.persistence.Id;
 
 import java.util.UUID;
 
-/**
- * The User class represents a user in the system.
- * It may include properties such as username, password, and other personal details,
- * and is designed to store and manage user-related information.
- *
- * This class can be used as a foundation for user authentication, authorization,
- * and other user-related features in an application.
- */
-
 @Entity
 public class User {
 
