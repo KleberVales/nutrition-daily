566e7a3cd4fe7f691d4f43d8ee0ca0dda395c4c7
Kleber Vales <klebervales.dev@gmail.com>
2026-05-15 17:19:39 -0300

    Annotate `User` entity with JPA annotations, add new fields, and update build.gradle with dependencies for JPA and Lombok

--- user-service/src/main/java/com/nutritiondaily/userservice/entity/User.java
+++ user-service/src/main/java/com/nutritiondaily/userservice/entity/User.java
@@ -1,14 +1,16 @@
 package com.nutritiondaily.userservice.entity;
 
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.util.UUID;
 
 @Entity
+@Table(name="users")
+@Getter
+@Setter
 public class User {
 
     @Id
