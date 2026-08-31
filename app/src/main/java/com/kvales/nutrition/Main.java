64d02323f97ebc3e4ac1ee895145b00e35cc3689
Kleber Vales <klebervales.dev@gmail.com>
2026-08-31 18:40:55 -0300

    feat: module app with settings and database

---
+++ app/src/main/java/com/kvales/nutrition/Main.java
@@ -0,0 +1,17 @@
+package com.kvales.nutrition;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+@SpringBootApplication
+public class Main {
+
+    public static void main(String[] args) {
+
+        ConfigurableApplicationContext context = SpringApplication.run(Main.class, args);
+
+    }
+
+}
