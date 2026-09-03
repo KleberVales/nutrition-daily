a1fa058cb111ddb78401f0940ca8802a9c2c0edf
Kleber Vales <klebervales.dev@gmail.com>
2026-09-03 16:49:00 -0300

    feat: create auth module with spring security and spring data

---
+++ auth/src/main/java/com/kvales/nutrition/application/port/in/AuthenticateUseCase.java
@@ -0,0 +1,19 @@
+package com.kvales.nutrition.application.port.in;
+
+public interface AuthenticateUseCase {
+
+    AuthenticateResult authenticate(AuthenticateCommand command);
+
+    record AuthenticateCommand(
+            String email,
+            String password
+    ) {
+    }
+
+    record AuthenticateResult(
+            String token
+    ) {
+    }
+
+
+}
